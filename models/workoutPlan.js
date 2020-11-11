ed90408dad55e71cf2aee9b841c5192e39dcb328
Delmy Arguello <kroarguelloadel@gmail.com>
2020-11-11 17:11:36 -0600

    adding Schema

--- models/workoutPlan.js
+++ models/workoutPlan.js
@@ -2,3 +2,30 @@
 
 const mongoose = require("mongoose");
 
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+
+    day:{
+    type: Date,
+    default: Date.now
+    },
+    exercise: [{
+       type: {},
+       name:{},
+       duration:{},
+       weight:{},
+       reps:{},
+       set:{},
+       distance:{},
+       resistanceDuration:{}
+
+    }]
+
+
+
+})
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = workout;
