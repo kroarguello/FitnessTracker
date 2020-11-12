5b219043bff30301861c3dac9b5358906c631e18
Delmy Arguello <kroarguelloadel@gmail.com>
2020-11-11 23:19:35 -0600

    adding api

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-//const db = require("./models");
+const db = require("./models");
 const path = require("path");
-//express
 
+//express
 const app = express();
 
 app.use(logger("dev"));
@@ -17,11 +17,14 @@ app.use(express.static("public"));
 
 // connection and Mongodb
 var PORT = process.env.PORT || 3000;
+
 //this is in seed.js
-//const Workout = require("./workoutPlan.js");
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 
+const { update } = require("./models/workoutPlan");
+const { Workout } = require("./models");
 
 // ROUTER
 
@@ -32,28 +35,53 @@ app.get("/exercise", ({ body }, res) => {
     
   });
 
- /* app.get("/exercise?", ({ body }, res) => {
-      console.log("continue workout");
-      res.sendFile(path.join(__dirname, "./public/exercise.html"))
-    //User.create(body)
-    //  .then(dbUser => {
-    //    res.json(dbUser);
-    //  })
-    //  .catch(err => {
-    //    res.json(err);
-    //  });
-  });
-*/
-  
-
-
-
   app.get("/stats", ({ body }, res) => {
     console.log("STATS");
     res.sendFile(path.join(__dirname, "./public/stats.html"))
     
   });
 
+ 
+
+
+ //api
+ 
+ app.post("/api/workouts", async (req, res) => {
+    const response = await db.Workout.create({ type: "workout" })
+    res.json(response);
+});
+
+app.put("/api/workouts/:id", (req, res) => {
+    const id = req.params.id;
+    console.log(req.body);
+    db.Workout.findByIdAndUpdate({ _id: id }, { $push: { exercises: req.body } }, { new: true })
+        .then(event => {
+            res.json(event);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+})
+
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+        .then(event => {
+            res.json(event);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+        .then(event => {
+            res.json(event);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
 
 
 // =============================================================================
