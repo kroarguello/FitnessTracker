28bbb096df685c7fdbe2196b61a872d82b9a001e
Delmy Arguello <kroarguelloadel@gmail.com>
2020-11-12 17:24:23 -0600

    deleting extra code

--- models/workout.js
+++ models/workout.js
@@ -21,19 +21,7 @@ const WorkoutSchema = new Schema({
       // resistanceDuration:{type: Number,trim:true, required:true}
 
     }]
-//}//,
-/*{
-  toJSON: {
-    virutals:true
-  }
-}
-);
 
-WorkoutSchema.virtual("duration").get(function () {
-  // "reduce" array of exercises down to just the sum of their durations
-  return this.exercises.reduce((total, exercise) => {
-    return total + exercise.duration;
-  }, 0);*/
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
